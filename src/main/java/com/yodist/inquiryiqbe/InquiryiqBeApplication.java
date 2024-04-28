3235e31c37aee6904d35f23a16990c45a0c96608
yodist <yodisatriani@live.com>
2024-04-28 21:10:40 +0700

    initial commit spring

---
+++ src/main/java/com/yodist/inquiryiqbe/InquiryiqBeApplication.java
@@ -0,0 +1,13 @@
+package com.yodist.inquiryiqbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiryiqBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiryiqBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yodist/inquiryiqbe/InquiryiqBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yodist.inquiryiqbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiryiqBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
