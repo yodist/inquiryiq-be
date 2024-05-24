2cea99a3bbe5d255171008e2b238c8acaae81eb5
Yodi Satriani <yodisatriani@live.com>
2024-05-24 14:12:12 +0700

    support search people also ask with serphouse

---
+++ src/main/java/com/yodist/inquiryiqbe/constant/Constant.java
@@ -0,0 +1,32 @@
+package com.yodist.inquiryiqbe.constant;
+
+public class Constant {
+    private Constant() {}
+
+    public static final String ENV_PRODUCTION = "production";
+
+    public static final String BODY_DATA = "data";
+    public static final String BODY_MESSAGE = "message";
+    public static final String BODY_ERROR_MESSAGE = "error_message";
+
+    public static final String SERPAPI_Q = "q";
+    public static final String SERPAPI_GOOGLE_DOMAIN = "google_domain";
+    public static final String SERPAPI_HL = "hl";
+    public static final String SERPAPI_GL = "gl";
+    public static final String SERPAPI_API_KEY = "api_key";
+
+    public static final String SERPAPI = "serpapi";
+    public static final String SERPHOUSE = "serphouse";
+
+    public static final String SERPHOUSE_Q = "q";
+    public static final String SERPHOUSE_DOMAIN = "domain";
+    public static final String SERPHOUSE_LOC = "loc";
+    public static final String SERPHOUSE_LANG = "lang";
+    public static final String SERPHOUSE_DEVICE = "device";
+    public static final String SERPHOUSE_SERPTYPE = "serp_type";
+    public static final String SERPHOUSE_VERBATIM = "verbatim";
+    public static final String SERPHOUSE_PAGE = "page";
+    public static final String SERPHOUSE_NUM_RESULT = "num_result";
+
+
+}
--- src/main/java/com/yodist/inquiryiqbe/service/RelatedQuestionService.java
+++ src/main/java/com/yodist/inquiryiqbe/service/RelatedQuestionService.java
@@ -1,10 +1,7 @@
 package com.yodist.inquiryiqbe.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import com.hw.serpapi.GoogleSearch;
 import com.yodist.inquiryiqbe.dto.RelatedQuestion;
 import com.yodist.inquiryiqbe.dto.request.RelatedQuestionReq;
@@ -14,6 +11,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -25,6 +25,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.yodist.inquiryiqbe.constant.Constant.*;
+
 @Service
 public class RelatedQuestionService {
 
@@ -32,50 +34,96 @@ public class RelatedQuestionService {
 
     @Value("${serpapi.key:test}")
     String serpapiKey;
-    @Value("${serpapi.uri:https://serpapi.com/search}")
-    String serpapiUri;
-    @Value("${serpapi.env:dummy}")
-    String serpapiEnv;
+    // valid values are "production" and "nonprod"
+    @Value("${data.env:nonprod}")
+    String dataEnv;
     @Value("${google.domain:google.com}")
     String googleDomain;
+    @Value("${serphouse.url:https://api.serphouse.com/serp/live}")
+    String serphouseUrl;
+    @Value("${serphouse.key:test}")
+    String serphouseKey;
+    @Value("${serp.engine:serphouse}")
+    String serpEngine;
 
     @Autowired
     RestTemplate restTemplate;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    ObjectMapper objectMapper;
 
     @Autowired
-    private TraceService traceService;
+    TraceService traceService;
 
     public ResponseEntity<Object> getRelatedQuestions(RelatedQuestionReq req) {
         final Map<String, Object> responseBody = ResponseBuilder.generateDefaultResponseBody(traceService.getCurrentTraceId());
         try {
             log.info("in method getRelatedQuestions");
 
-            Map<String, String> parameter = new HashMap<>();
-            parameter.put("q", req.getKeyword());
-            parameter.put("google_domain", googleDomain);
-            parameter.put("hl", req.getLanguage());
-            parameter.put("gl", req.getCountry());
-            parameter.put("api_key", serpapiKey);
-            log.debug("serpapi.key: {}", serpapiKey);
+            List<RelatedQuestion> questionList = new ArrayList<>();
 
-            JsonObject jsonObject;
-            if (isProduction()) {
-                jsonObject = getJsonObjectFromGoogle(parameter);
+            if (SERPAPI.equals(serpEngine)) {
+                log.info("use SERPAPI");
+                JsonObject jsonObject;
+                if (isProduction()) {
+                    Map<String, String> parameter = new HashMap<>();
+                    parameter.put(SERPAPI_Q, req.getKeyword());
+                    parameter.put(SERPAPI_GOOGLE_DOMAIN, googleDomain);
+                    parameter.put(SERPAPI_HL, req.getLanguage());
+                    parameter.put(SERPAPI_GL, req.getCountry());
+                    parameter.put(SERPAPI_API_KEY, serpapiKey);
+                    log.debug("serpapi.key: {}", serpapiKey);
+                    jsonObject = getJsonObjectFromGoogleSerpapi(parameter);
+                } else {
+                    jsonObject = getJsonObjectFromDummyFile();
+                }
+                questionList = getRelatedQuestionsFromJsonObject(jsonObject);
+                setDummySubQuestion(questionList);
+            } else if (SERPHOUSE.equals(serpEngine)) {
+                log.info("use SERPHOUSE");
+                JsonObject jsonObject;
+                if (isProduction()) {
+                    Map<String, Object> requestData = new HashMap<>();
+                    requestData.put(SERPHOUSE_Q, req.getKeyword());
+                    requestData.put(SERPHOUSE_DOMAIN, googleDomain);
+                    requestData.put(SERPHOUSE_LOC, req.getCountry());
+                    requestData.put(SERPHOUSE_LANG, req.getLanguage());
+                    requestData.put(SERPHOUSE_DEVICE, "desktop");
+                    requestData.put(SERPHOUSE_SERPTYPE, "web");
+                    requestData.put(SERPHOUSE_PAGE, "1");
+                    requestData.put(SERPHOUSE_VERBATIM, "0");
+                    requestData.put(SERPHOUSE_NUM_RESULT, "10");
+                    Map<String, Object> wrapper = new HashMap<>();
+                    wrapper.put("data", requestData);
+                    // Set headers with Content-Type
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                    headers.setBearerAuth(serphouseKey);
+                    String wrapperString = objectMapper.writeValueAsString(wrapper);
+                    // Create a HttpEntity with the request data and headers
+                    HttpEntity<String> request = new HttpEntity<>(wrapperString, headers);
+                    log.debug(wrapperString);
+                    // Send the POST request
+                    ResponseEntity<String> response = restTemplate.postForEntity(serphouseUrl, request, String.class);
+                    log.debug(String.valueOf(response));
+                    jsonObject = getJsonObjectFromGoogleSerphouse(response.getBody());
+                } else {
+                    jsonObject = getJsonObjectFromDummyFile();
+                }
+                questionList = getRelatedQuestionsFromJsonObjectGoogleSerphouse(jsonObject);
             } else {
-                jsonObject = getJsonObjectFromDummyFile();
+                // TODO: will need to implement this, get data directly from google and bypass captcha
+                JsonObject jsonObject = getJsonObjectFromGoogle(null);
+                log.debug("getJsonObjectFromGoogle {}", jsonObject);
             }
-            List<RelatedQuestion> questionList = getRelatedQuestionsFromJsonObject(jsonObject);
-            setDummySubQuestion(questionList);
 
-            responseBody.put("data", questionList);
-            responseBody.put("message", "everything is alright");
+            responseBody.put(BODY_DATA, questionList);
+            responseBody.put(BODY_MESSAGE, "everything is alright");
             log.debug(objectMapper.writeValueAsString(responseBody));
         } catch (Exception ex) {
             log.error("error when trying to get related question data: {}", ex.getMessage());
-            responseBody.put("errorMessage", "something is wrong!");
+            log.trace("error", ex);
+            responseBody.put(BODY_ERROR_MESSAGE, "something is wrong!");
             return ResponseEntity.internalServerError().body(responseBody);
         }
         return ResponseEntity.ok(responseBody);
@@ -101,11 +149,33 @@ public class RelatedQuestionService {
         return jsonObject;
     }
 
-    private static JsonObject getJsonObjectFromGoogle(Map<String, String> parameter) {
+    private static JsonObject getJsonObjectFromGoogleSerpapi(Map<String, String> parameter) {
         JsonObject jsonObject;
         GoogleSearch search = new GoogleSearch(parameter);
         jsonObject = search.getJson();
         return jsonObject;
+    }
+
+    private static JsonObject getJsonObjectFromGoogleSerphouse(String response) {
+        Gson gson = new Gson();
+        return gson.fromJson(response, JsonObject.class);
+    }
+
+    private static List<RelatedQuestion> getRelatedQuestionsFromJsonObjectGoogleSerphouse(JsonObject jsonObject) {
+        List<RelatedQuestion> questionList = new ArrayList<>();
+        JsonObject results = jsonObject.getAsJsonObject("results").getAsJsonObject("results");
+        JsonArray peopleAlsoAskList = results.getAsJsonArray("people_also_ask");
+        for (JsonElement el : peopleAlsoAskList) {
+            RelatedQuestion question = new RelatedQuestion();
+            String questionValue = el.getAsJsonObject().get("question").getAsString();
+            question.setQuestion(questionValue);
+            questionList.add(question);
+        }
+        return questionList;
+    }
+
+    private JsonObject getJsonObjectFromGoogle(Map<String, String> parameter) {
+        return new JsonObject();
     }
 
     private void setDummySubQuestion(final List<RelatedQuestion> questionList) {
@@ -124,7 +194,7 @@ public class RelatedQuestionService {
     }
 
     private boolean isProduction() {
-        return "production".equals(serpapiEnv);
+        return ENV_PRODUCTION.equals(dataEnv);
     }
 
     private static String getSampleJson() throws IOException {
